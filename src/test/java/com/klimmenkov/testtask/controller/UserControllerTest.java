760597645dc6c697343f8e571a673d776cba606c
Klim Menkov <klim.menkov@ukr.net>
2023-09-28 01:58:40 +0300

    Code refactoring

--- src/test/java/com/klimmenkov/testtask/controller/UserControllerTest.java
+++ src/test/java/com/klimmenkov/testtask/controller/UserControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Calendar;
@@ -150,7 +149,7 @@ public class UserControllerTest {
 
         String userJson = asJsonString(user);
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/users/create")
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/create")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(userJson))
                 .andExpect(status().isBadRequest())
